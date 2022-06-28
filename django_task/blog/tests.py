f2ec414cc055a62e47064bcaf1cbd9422005a250
Rajat Shukla <86393262+rajatshukla009@users.noreply.github.com>
2022-06-28 08:30:38 +0530

    Update tests.py

--- django_task/blog/tests.py
+++ django_task/blog/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.shortcuts import reverse
+from.models import Post
 
-# Create your tests here.
+
+class URLTests(SimpleTestCase):
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_name(self):
+        response = self.client.get(reverse('blog-home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_corrrect_template(self):
+        response = self.client.get(reverse('blog-home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,'blog/home.html')
+
+    def test_aboutpage(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_name(self):
+        response = self.client.get(reverse('about-blog'))
+        self.assertEqual(response.status_code,200)
+
+    def test_corrrect_template(self):
+        response = self.client.get(reverse('about-blog'))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'blog/about.html')
+
+
+class PostTests(TestCase):
+
+    @classmethod
+    def setUp(cls):
+        Post.objects.create(title="this is a test")
+
+    def tests_text(self):
+        post = Post.objects.get(id=1)
+        expected_post_title = post.title
+        self.assertEqual(expected_post_title,"this is a test")
+
+    def test_post_list(self):
+        response = self.client.get(reverse('posts'))
+        self.assertEquals(response.status_code,200)
+        self.assertContains(response, "this is a test")
+        self.assertTemplateUsed(response,'blog/user_posts.html')
